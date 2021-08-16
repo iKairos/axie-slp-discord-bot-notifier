eb1f344ccc987a860ddd8054d3da0d47545ce37a
student-iKairos <johnchris.torres@tup.edu.ph>
2021-08-16 10:58:54 +0800

    initial slp rate command

--- cogs/slp.py
+++ cogs/slp.py
@@ -1,6 +1,7 @@
 import discord, random
 from discord.ext import commands
 from pycoingecko import CoinGeckoAPI
+from datetime import datetime
 
 cg = CoinGeckoAPI()
 
@@ -18,7 +19,26 @@ class SLP(commands.Cog):
     async def rate(self, ctx, currency):
         price = float(get_slpprice(currency)['smooth-love-potion'][currency]) 
 
-        await ctx.send(f"The SLP rate for {currency} is {price}")
+        symbol = cg.get_exchange_rates()['rates'][currency]['unit']
+
+        embed = discord.Embed(
+            description=f"The current SLP rate for currency {currency.upper()}.",
+            color=0x1ABC9C,
+            timestamp=datetime.utcnow()
+        )
+        embed.set_author(
+            name="Smooth Love Potion Rates",
+            icon_url="https://cdn.discordapp.com/attachments/545533157131288587/876658614553686016/slp.png"
+        )
+        embed.add_field(
+            name=f"SLP Rate to {currency}",
+            value=f"{symbol}{price}"
+        )
+        embed.set_footer(
+            text="SLP Rate as of"
+        )
+
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(SLP(bot))

2dd3bf199dd12a2f5d1db7713041419f467ba662
student-iKairos <johnchris.torres@tup.edu.ph>
2021-08-16 10:26:58 +0800

    initial rate code

--- cogs/slp.py
+++ cogs/slp.py
@@ -5,6 +5,11 @@ class SLP(commands.Cog):
     def __init__(self, bot):
         self.bot = bot 
     
-    
+    @commands.command(name="rate", 
+        description="Displays the SLP Rate for the specified currency.",
+        usage="slp rate <currency>")
+    async def rate(self, ctx, currency):
+        pass
+
 def setup(bot):
     bot.add_cog(SLP(bot))

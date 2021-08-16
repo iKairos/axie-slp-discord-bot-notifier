8e6d8e0e56cbf6102744138c601730a356b563d9
student-iKairos <johnchris.torres@tup.edu.ph>
2021-08-16 12:55:59 +0800

    initial graph command commit, tweaks in rate command

--- cogs/slp.py
+++ cogs/slp.py
@@ -1,13 +1,8 @@
 import discord, random
 from discord.ext import commands
-from pycoingecko import CoinGeckoAPI
 from datetime import datetime
-
-cg = CoinGeckoAPI()
-
-def get_slpprice(currency): # function to retrieve specifically SLP price rate
-    price = cg.get_price(ids='smooth-love-potion', vs_currencies=currency)
-    return(price)
+from pycoingecko import CoinGeckoAPI
+from utils.slp_helpers import *
 
 class SLP(commands.Cog):
     def __init__(self, bot):
@@ -17,9 +12,15 @@ class SLP(commands.Cog):
         description="Displays the SLP Rate for the specified currency.",
         usage="slp rate <currency>")
     async def rate(self, ctx, currency):
+        await ctx.message.channel.trigger_typing()
+
+        cg = CoinGeckoAPI()
         price = float(get_slpprice(currency)['smooth-love-potion'][currency]) 
 
         symbol = cg.get_exchange_rates()['rates'][currency]['unit']
+
+        past = get_minutely_market_history(currency)
+        past = past[len(past)-2]
 
         embed = discord.Embed(
             description=f"The current SLP rate for currency {currency.upper()}.",
@@ -31,14 +32,42 @@ class SLP(commands.Cog):
             icon_url="https://cdn.discordapp.com/attachments/545533157131288587/876658614553686016/slp.png"
         )
         embed.add_field(
-            name=f"SLP Rate to {currency}",
+            name=f"Current SLP to {currency.upper()} rate",
             value=f"{symbol}{price}"
         )
+        embed.add_field(
+            name=f"Direction",
+            value="⬆" if past < price else "⬇"
+        )
         embed.set_footer(
-            text="SLP Rate as of"
+            text="Rate as of"
         )
 
         await ctx.send(embed=embed)
+    
+    @commands.command(aliases=['hm'],
+        name="historyminutely", 
+        description="Displays the minutely graph of SLP prices.",
+        usage="slp historyminutely <currency>")
+    async def historyminutely(self, ctx, currency):
+        await ctx.message.channel.trigger_typing()
+
+        graph_day_market_history(currency)
+
+        file = discord.File("assets/images/graphs/minutely.png", filename="mnt.png")
+
+        embed = discord.Embed(
+            title="SLP Minutely Behavior",
+            description="Graph of the minutely behavior of SLP today.",
+            color=0x1ABC9C,
+            timestamp=datetime.utcnow()
+        )
+        embed.set_image(url="attachment://mnt.png")
+        embed.set_footer(
+            text="Graph as of"
+        )
+
+        await ctx.send(file=file, embed=embed)
 
 def setup(bot):
     bot.add_cog(SLP(bot))
---
+++ utils/slp_helpers.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+from pycoingecko import CoinGeckoAPI
+
+def get_slpprice(currency): # function to retrieve specifically SLP price rate
+    cg = CoinGeckoAPI()
+    price = cg.get_price(ids='smooth-love-potion', vs_currencies=currency)
+    return price
+
+def get_minutely_market_history(currency):
+    cg = CoinGeckoAPI()
+
+    market_history = cg.get_coin_market_chart_by_id(id="smooth-love-potion", vs_currency=currency, days=1)
+
+    prices = market_history['prices']
+
+    temp = []
+
+    for volume, price in prices:
+        temp.append(price)
+    
+    return temp
+
+def graph_day_market_history(currency): # graphs minutely data of slp 
+    temp = get_minutely_market_history(currency)
+    
+    x = list(range(len(temp)))
+    plt.plot(x, temp, label=f"SLP per Minute ({currency.upper()})")
+    plt.legend()
+    plt.xlabel("Minute")
+    plt.ylabel(f"SLP Price in {currency.upper()}")
+    plt.title("SLP Price Minutely Data")
+    plt.text(x[len(x)-1], temp[len(temp)-1], f"{temp[len(temp)-1]:.2f}", rotation=45)
+    plt.grid(color = 'black', linestyle = '--', linewidth = 0.5)
+
+    plt.savefig("assets/images/graphs/minutely.png")

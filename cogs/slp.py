93b19464647d8cd6d5d00a7551fc9d1f0f79f55e
student-iKairos <johnchris.torres@tup.edu.ph>
2021-08-16 10:17:04 +0800

    cleaned placeholder

--- cogs/slp.py
+++ cogs/slp.py
@@ -5,11 +5,6 @@ class SLP(commands.Cog):
     def __init__(self, bot):
         self.bot = bot 
     
-    @commands.command(name="say", 
-        description="Repeats what you say.",
-        usage="temp")
-    async def say(self, ctx, *, message=None):
-        await ctx.send(message)
     
 def setup(bot):
     bot.add_cog(SLP(bot))

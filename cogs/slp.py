49015ab675173e1ef0751a235f1de6033cd97cf3
lucitian <flippery.slippery28@gmail.com>
2021-08-16 10:42:57 +0800

    added retrieval function of slp price rate

--- cogs/slp.py
+++ cogs/slp.py
@@ -1,5 +1,12 @@
 import discord, random
 from discord.ext import commands
+from pycoingecko import CoinGeckoAPI
+
+cg = CoinGeckoAPI()
+
+def get_slpprice(currency): # function to retrieve specifically SLP price rate
+    price = cg.get_price(ids='smooth-love-potion', vs_currencies=currency)
+    return(price)
 
 class SLP(commands.Cog):
     def __init__(self, bot):
@@ -9,7 +16,9 @@ class SLP(commands.Cog):
         description="Displays the SLP Rate for the specified currency.",
         usage="slp rate <currency>")
     async def rate(self, ctx, currency):
-        pass
+        price = float(get_slpprice(currency)['smooth-love-potion'][currency]) 
+
+        await ctx.send(f"The SLP rate for {currency} is {price}")
 
 def setup(bot):
     bot.add_cog(SLP(bot))
